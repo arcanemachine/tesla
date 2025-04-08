aad43a587db266fa90479d858cd512256b66edde
Nicholas Moen <arcanemachine@gmail.com>
2025-04-08 12:43:32 -0600

    fix: do not add content-type header in json middleware if it already exists

--- lib/tesla/middleware/json.ex
+++ lib/tesla/middleware/json.ex
@@ -82,10 +82,17 @@ defmodule Tesla.Middleware.JSON do
   def encode(env, opts) do
     with true <- encodable?(env),
          {:ok, body} <- encode_body(env.body, opts) do
-      {:ok,
-       env
-       |> Tesla.put_body(body)
-       |> Tesla.put_headers([{"content-type", encode_content_type(opts)}])}
+      env_has_content_type_header =
+        "content-type" in (env.headers |> Enum.map(fn {k, _v} -> String.downcase(k) end))
+
+      if env_has_content_type_header do
+        {:ok, env |> Tesla.put_body(body)}
+      else
+        {:ok,
+         env
+         |> Tesla.put_body(body)
+         |> Tesla.put_headers([{"content-type", encode_content_type(opts)}])}
+      end
     else
       false -> {:ok, env}
       error -> error
